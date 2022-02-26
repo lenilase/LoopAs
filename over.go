0a0f15cc1eacf67e7a3c4e40c0a9de89e0e7d322
lenilase <leni.lase@student.uh.ac.id>
2022-02-26 20:44:32 +0700

    Section_4

--- over.go
+++ over.go
@@ -5,11 +5,11 @@ import "fmt"
 func main(){
 	//LOOPING OVER STRING
 	sentence := "Hello"
-	for i := 0; i < len(sentence); i++ {
-		fmt.Printf(string(sentence[i]) + "-")
+	for i := 0; i < len(sentence) ; i++{
+	fmt.Printf(string(sentence[i]) + "-")
 	}
-	fmt.println("   ------>>>")
-	for pos, char := range sentence {
-	fmt.printf("Character %c start at byte position %d/n", char, pos)
+	fmt.Println("   ------>>>")
+	for pos, char := range sentence{
+		fmt.Printf("Character %c start at byte position %d\n", char, pos)
 	}
 }
